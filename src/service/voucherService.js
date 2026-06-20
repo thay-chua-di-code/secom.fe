0c38011722d61feb555e895564952dc3853d1682
lelong2507 <hoanglong6939tcv@gmail.com>
2026-06-20 20:20:36 +0700

    feat(vouchers): add voucher data structure and service for fetching vouchers

---
+++ src/service/voucherService.js
@@ -0,0 +1,15 @@
+import axiosClient from "../api/axiosClient";
+
+const BASE = "/vouchers";
+
+export const voucherService = {
+  getAll: async () => {
+    const res = await axiosClient.get(BASE);
+    return res.data;
+  },
+
+  getByCode: async (code) => {
+    const res = await axiosClient.get(`${BASE}?code=${code}`);
+    return res.data?.[0] || null;
+  },
+};
