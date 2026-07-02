0569509dd6a278641c89b4e35edf0b157dd9d135
lelong2507 <hoanglong6939tcv@gmail.com>
2026-07-02 23:42:54 +0700

    fix(PrivateRoute): streamline authentication checks and improve login redirection

--- src/guards/PrivateRoute.jsx
+++ src/guards/PrivateRoute.jsx
@@ -5,6 +5,13 @@ import { useSelector } from "react-redux";
 export default function PrivateRoute() {
   const token = localStorage.getItem("token");
   const role = useSelector((state) => state.auth.role);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  } else {
+    return <Navigate to="/login" replace />;
+  }
   if (!isLoggedIn(token)) {
     return <Navigate to="/login" replace />;
   }
--- src/utils/fncUtils.js
+++ src/utils/fncUtils.js
@@ -1,8 +1,11 @@
 // Change money to VietNam
 export const formatCurrencyVN = (value) => {
-  if (!value) return "0";
+  if (value == null) return "0 ₫";
 
-  return Number(value).toLocaleString("vi-VN");
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  }).format(value);
 };
 
 export const formatDate = (date) => new Date(date).toLocaleDateString("vi-VN");
